1f60d9d03faf810c8bec2586210795c16acafeb3
Naptax <naptax@proton.me>
2022-11-20 15:42:36 +0100

    Refectoring of cmake chaintools

---
+++ Bof/CMakeFiles/Bof-tuto.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/naptax/code/Bof/bof-1.c" "CMakeFiles/Bof-tuto.dir/bof-1.c.o" "gcc" "CMakeFiles/Bof-tuto.dir/bof-1.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
