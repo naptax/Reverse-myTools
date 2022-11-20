c73d584d10b459dcb920b388159eb4a8e72d4ffb
Naptax <naptax@proton.me>
2022-11-20 21:21:12 +0100

    Change

---
+++ BUF/CMakeFiles/BUF.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BUF.dir/main.c.o"
+  "CMakeFiles/BUF.dir/main.c.o.d"
+  "bin/LINUX-ELF/BUF"
+  "bin/LINUX-ELF/BUF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BUF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- BUF/main.c
+++ BUF/main.c
@@ -5,16 +5,30 @@
 #include <string.h>
 
 
+void donne(int montant, char *ongName){  
+   char ong[15];         
+   
+   strcpy(ong, ongName);    // Ici on copie une chaine de caracteres dans une zone memoire de 15
+                            // On va donc deborder sur le contenu qui suit juste derriere dans la stack
+                            // et comme on appelle une fonction (printf), ce qui suit juste derriere ONG,
+                            // c'est la sauvegarde de EIP avant le CALL de printf 
+
+                            // So ==> je peux ecrire une adresse de mon choix en ecrasant la valeur a cette adresse et sera le 
+                            // nouvel EIP au moment du return du print
+                            // QUESTION : QUELLE ADRESSE
+
+   printf("Vous avez fait un don de %d euros a l'ONG %s.\n", montant, ong);
+}
+
 
 int main(int argc, char *argv[]){
-    char ong[8];
+    char ong[15];
     int montant = 1000;
 
     if (argc==2) {
-        strcpy(ong, argv[1]);        // Aie, aie, aie
-        printf("Vous avez fait un don de %d euros a l'ONG %s.\n", montant, ong);
-        printf("Un grand MERCI !\n");
-        return 0;
+        donne(1000, argv[1]);
+        printf("Un grand MERCI !\n");                        
+        exit(0);
     }
     else
     {
@@ -24,3 +38,7 @@ int main(int argc, char *argv[]){
 
 
 }
+
+
+
+  
