d7f1961f3e13edffcce5179511ef0bfa4156b167
Naptax <naptax@proton.me>
2022-11-20 17:51:33 +0100

    Change

--- BUF/main.c
+++ BUF/main.c
@@ -5,23 +5,22 @@
 #include <string.h>
 
 
-void donner(char *ongName, int montant) {
-    printf("Vous avez fait un don de %d euros a l'ONG %s.\n", montant, ongName);
-}
 
 int main(int argc, char *argv[]){
     char ong[8];
     int montant = 1000;
 
-    if (argc!=1)
-        strcpy(ong,argv[1]);        // Aie, aie, aie
+    if (argc==2) {
+        strcpy(ong, argv[1]);        // Aie, aie, aie
+        printf("Vous avez fait un don de %d euros a l'ONG %s.\n", montant, ong);
+        printf("Un grand MERCI !\n");
+        return 0;
+    }
     else
     {
         printf("\n\tusage: %s <destinataire>\n\n",argv[0]);
         exit(0);
     }
 
-    donner(ong, montant);
-    printf("Un grand MERCI\n");
-    return 0;
+
 }
