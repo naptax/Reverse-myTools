c64fda643622e8814019a207c57759b5eef05c75
Naptax <naptax@proton.me>
2022-11-23 18:11:11 +0100

    exploit fix

--- BUF/main.c
+++ BUF/main.c
@@ -1,28 +1,13 @@
-// Buffer Over Flow
-// Exemple I
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 
-
 void donne(int montant){  
-   char ong[15];         
+   char ong[256];         
    
    printf("A quelle ONG spuhaitez-vous faire un don ? \n");
    gets(ong);
-
-/*
-   strcpy(ong, ongName);    // Ici on copie une chaine de caracteres dans une zone memoire de 15
-                            // On va donc deborder sur le contenu qui suit juste derriere dans la stack
-                            // et comme on appelle une fonction (printf), ce qui suit juste derriere ONG,
-                            // c'est la sauvegarde de EIP avant le CALL de printf 
-
-                            // So ==> je peux ecrire une adresse de mon choix en ecrasant la valeur a cette adresse et sera le 
-                            // nouvel EIP au moment du return du print
-                            // QUESTION : QUELLE ADRESSE
-*/
-
    printf("Vous avez fait un don de %d euros a l'ONG %s.\n", montant, ong);
 }
 
@@ -31,7 +16,7 @@ int main(int argc, char *argv[]){
     int montant = 1000;
 
         donne(montant);
-        printf("Un grand MERCI !\n");                        
+        printf("Un grand MERCI cher donnateur !\n");                        
         exit(0);
     }
 
