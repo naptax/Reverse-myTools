ea0f76ea2900ebaf82a3c9386c124ff16d9b8012
NaPtaX <naptax@proton.me>
2023-02-09 22:43:28 +0100

    add ShellConfig x86 and x64

--- RUST/shellExec/src/main.rs
+++ RUST/shellExec/src/main.rs
@@ -1,23 +1,20 @@
-use std::fs::File;
-use std::io::Read;
-use winapi;
+use windows::{
+    core::*, Data::Xml::Dom::*, Win32::Foundation::*, Win32::System::Threading::*,
+    Win32::UI::WindowsAndMessaging::*,
+};
 
-unsafe extern "system" fn shellcode_execute() -> u8 {
-    let mut shellbuffer = Vec::new();
+fn main() -> Result<()> {
 
-    let mut file = File::open("shellcode.bin").unwrap();
-    file.read_to_end(&mut shellbuffer).unwrap();
 
-    let mut allocated: *mut c_void = VirtualAlloc(std::ptr::null_mut(), shellbuffer.len(), MEM_COMMIT, PAGE_READWRITE);
+    unsafe {
+        let event = CreateEventW(None, true, false, None)?;
+        SetEvent(event).ok()?;
+        WaitForSingleObject(event, 0);
+        CloseHandle(event).ok()?;
 
-/*    CopyMemory(allocated, shellbuffer.as_ptr() as *const c_void, shellbuffer.len());
+        MessageBoxA(None, s!("Ansi"), s!("Caption"), MB_OK);
+        MessageBoxW(None, w!("Wide"), w!("Caption"), MB_OK);
+    }
 
-    let mut old_protect: DWORD = 0;
-    VirtualProtect(allocated, shellbuffer.len(), PAGE_EXECUTE_READ, &mut old_protect);
-
-    let h_thread = CreateThread(std::ptr::null_mut(), 0, allocated as *mut c_void, std::ptr::null_mut(), 0, std::ptr::null_mut());
-
-    WaitForSingleObject(h_thread as *mut c_void, -1); */
-
-    1
+    Ok(())
 }
